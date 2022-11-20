fb37f5def00af79de22af16114af439a93f54379
Sudarshan <sudarshan.vyas.11@gmail.com>
2022-11-20 15:28:14 +0000

    SV - Open API maven configuration

---
+++ src/main/java/com/example/openapi/OpenApiLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.example.openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiLibraryApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/openapi/controller/BookController.java
@@ -0,0 +1,8 @@
+package com.example.openapi.controller;
+
+import com.example.openapi.api.BookApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController implements BookApi {
+}
