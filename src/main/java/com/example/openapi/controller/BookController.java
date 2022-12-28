9a1c96506df8f5766b5b286c6069ca737e7f6042
Sudarshan <sudarshan.vyas.11@gmail.com>
2022-12-28 19:01:18 +0000

    SV - Added spring cloud to maven config plus upgrade to spring boot version 3

--- src/main/java/com/example/openapi/controller/BookController.java
+++ src/main/java/com/example/openapi/controller/BookController.java
@@ -1,8 +1,37 @@
 package com.example.openapi.controller;
 
-import com.example.openapi.api.BookApi;
+import com.example.openapi.api.DefaultApi;
+import com.example.openapi.model.Author;
+import com.example.openapi.model.Book;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class BookController implements BookApi {
+public class BookController implements DefaultApi {
+    @Override
+    public ResponseEntity<Book> getBookById(final Long id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Book> getBookByName(final String name) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Book>> listBooks() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Book>> listBooksByAuthor(final Author author) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Book>> listBooksByGenre(final String genre) {
+        return null;
+    }
 }
