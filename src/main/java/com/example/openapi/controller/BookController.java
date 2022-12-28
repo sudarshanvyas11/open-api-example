589703cd21871dc5a4a64896f04ecc8f45efde41
Sudarshan <sudarshan.vyas.11@gmail.com>
2022-12-28 19:33:04 +0000

    SV - Added tags for api generation

--- src/main/java/com/example/openapi/controller/BookController.java
+++ src/main/java/com/example/openapi/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.example.openapi.controller;
 
-import com.example.openapi.api.DefaultApi;
-import com.example.openapi.model.Author;
+import com.example.openapi.api.BookApi;
 import com.example.openapi.model.Book;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class BookController implements DefaultApi {
+public class BookController implements BookApi {
     @Override
     public ResponseEntity<Book> getBookById(final Long id) {
         return null;
@@ -17,21 +16,6 @@ public class BookController implements DefaultApi {
 
     @Override
     public ResponseEntity<Book> getBookByName(final String name) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<List<Book>> listBooks() {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<List<Book>> listBooksByAuthor(final Author author) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<List<Book>> listBooksByGenre(final String genre) {
         return null;
     }
 }
