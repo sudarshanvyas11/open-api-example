2435726231ac022cc6d69098b15391dda201150b
Sudarshan <sudarshan.vyas.11@gmail.com>
2023-01-05 12:31:12 +0000

    SV - Added h2 configuration

---
+++ src/test/java/com/example/openapi/config/H2FlywayConfig.java
@@ -0,0 +1,30 @@
+package com.example.openapi.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("test")
+public class H2FlywayConfig {
+
+    private static final String BOOK_SCRIPTS_LOCATION = "scripts/book";
+
+    @Bean
+    public String[] flywayLocations() {
+        return new String[]{BOOK_SCRIPTS_LOCATION};
+    }
+
+    @Bean
+    public Flyway flyway(final DataSource h2DataSource,
+                         final String[] flywayLocations) {
+        return Flyway.configure()
+                .defaultSchema("library")
+                .dataSource(h2DataSource)
+                .locations(flywayLocations)
+                .load();
+    }
+}
